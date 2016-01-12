dc831a3ce019edc64e9f80b33338fabcd6bd345d
Mark-A-A <markawai01@gmail.com>
2016-01-12 15:45:07 -0500

    Added verification modal and JS. JSLint messes JS format and structure

--- click.js
+++ click.js
@@ -1,26 +1,28 @@
-var counter = 0;
+$(document).ready(function(){
 
-function countup() {
-  counter = counter + 1;
- 
-}
+  var counter = 0;
 
-$("img").on('click', function(){  //on click of the image
-  countup();                //run the counter function to increase score
-  //$(this).prop('onclick',null);     // add null property = "this.onclick=null"
-  $(this).off('click');
+  function countup() {
+    counter += 1;
+  }
+
+  $("img").on('click', function (){  //on click of the image
+    countup();                //run the counter function to increase score
+    //$(this).prop('onclick',null);     // add null property = "this.onclick=null"
+    $(this).off('click');               // removed on click
+    });
+
+  //onclick="countdown()"
+  $("#start-button").on("click", function () {
+    countdown();
+    });
+
+    function countdown() {
+        setTimeout(function countandgive() {
+        //alert("Time's Up! You were able to click " + counter + " Icons!");
+          $('.modal-body').text("Time's Up! You were able to click " + counter + " Icons!");
+          $('#myModal').modal('show');
+        }, 20000);
+
+    }
 });
-
-//onclick="countdown()"
-$("#start-button").on("click",function(){
-  countdown();
-});
-
-function countdown() {
-  setTimeout(function countandgive()
-    { 
-    alert("Time's Up! You were able to click " +counter+ " Icons!");
-     
-     }, 20000);
-
-}
